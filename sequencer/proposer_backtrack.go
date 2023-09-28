0f2a2082379697995fb0f90c383c6ae78de3ded9
lunfardo314 <evaldas.drasutis@iota.org>
2023-09-28 09:52:35 +0300

    fixed BTrack

--- sequencer/proposer_backtrack.go
+++ sequencer/proposer_backtrack.go
@@ -84,7 +84,7 @@ func (b *backtrackProposer) generateCandidate(extend utangle.WrappedOutput) *tra
 
 	b.trace("trying extend %s to endorse %s", extend.VID.IDShort(), b.endorse.IDShort())
 
-	targetDelta, conflict, consumer := b.endorse.StartNextSequencerMilestoneDelta()
+	targetDelta, conflict, consumer := b.endorse.StartNextSequencerMilestoneDelta(extend.VID)
 	if conflict != nil {
 		b.trace("CANNOT extend %s to endorse: %s due to %s (consumer %s)",
 			extend.VID.IDShort(), b.endorse.IDShort(), conflict.DecodeID().Short(), consumer.IDShort())
