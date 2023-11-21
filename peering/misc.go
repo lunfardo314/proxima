2dff579325f275f63b98c6fbe6821b99892c7074
lunfardo314 <evaldas.drasutis@iota.org>
2023-11-21 10:36:40 +0200

    messaging skeleton

--- peering/misc.go
+++ peering/misc.go
@@ -1,6 +1,7 @@
 package peering
 
 import (
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"fmt"
@@ -8,8 +9,41 @@ import (
 	"math"
 
 	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/lunfardo314/proxima/core"
 	"github.com/lunfardo314/proxima/util"
+	"github.com/lunfardo314/unitrie/common"
 )
+
+func encodePeerMsgPull(txids ...core.TransactionID) []byte {
+	util.Assertf(len(txids) < math.MaxUint16, "too many transaction IDs")
+
+	var buf bytes.Buffer
+	var size [2]byte
+	binary.BigEndian.PutUint16(size[:], uint16(len(txids)))
+	buf.Write(size[:])
+	for i := range txids {
+		buf.Write(txids[i][:])
+	}
+	return buf.Bytes()
+}
+
+func decodePeerMsgPull(data []byte) ([]core.TransactionID, error) {
+	if len(data) < 2 {
+		return nil, fmt.Errorf("not a QueryTransactions message")
+	}
+	ret := make([]core.TransactionID, binary.BigEndian.Uint16(data[:2]))
+	rdr := bytes.NewReader(data[2:])
+	var txid [core.TransactionIDLength]byte
+	for i := range ret {
+		n, err := rdr.Read(txid[:])
+		if err != nil || n != core.TransactionIDLength {
+			return nil, fmt.Errorf("DecodePeerMessageQueryTransactions: wrong msg data")
+		}
+		ret[i], err = core.TransactionIDFromBytes(txid[:])
+		common.AssertNoError(err)
+	}
+	return ret, nil
+}
 
 func readFrame(stream network.Stream) ([]byte, error) {
 	var sizeBuf [4]byte
