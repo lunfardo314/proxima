74b696bd31903c3e8b6a88639908fdcb440c6015
lunfardo314 <evaldas.drasutis@iota.org>
2024-02-13 12:46:35 +0200

    cleanup

--- ledger/tests/genesis_test.go
+++ ledger/tests/genesis_test.go
@@ -1,10 +1,11 @@
-package multistate
+package tests
 
 import (
 	"crypto/ed25519"
 	"testing"
 
 	"github.com/lunfardo314/proxima/ledger"
+	"github.com/lunfardo314/proxima/multistate"
 	"github.com/lunfardo314/proxima/util/testutil"
 	"github.com/lunfardo314/unitrie/common"
 	"github.com/stretchr/testify/require"
@@ -14,13 +15,13 @@ func TestOriginBase(t *testing.T) {
 	const supply = 10_000_000_000
 	addr := ledger.AddressED25519FromPrivateKey(testutil.GetTestingPrivateKey())
 	genesisTimeSlot := ledger.Slot(1337)
-	gOut := InitialSupplyOutput(supply, addr, genesisTimeSlot)
+	gOut := multistate.InitialSupplyOutput(supply, addr)
 	t.Logf("Genesis: suppy = %d, genesis slot = %d:\n", supply, genesisTimeSlot)
 	t.Logf("   Genesis outputID: %s", gOut.ID.String())
 	t.Logf("   Genesis chain ID: %s", gOut.ChainID.String())
 	t.Logf("   Genesis output constraints:\n%s", gOut.Output.ToString("        "))
 
-	sOut := GenesisStemOutput(genesisTimeSlot)
+	sOut := multistate.GenesisStemOutput()
 	t.Logf("   Stem outputID: %s", sOut.ID.String())
 	t.Logf("   Stem output constraints:\n%s", sOut.Output.ToString("        "))
 
@@ -35,23 +36,23 @@ func TestInitOrigin(t *testing.T) {
 	privateKey := testutil.GetTestingPrivateKey()
 	id := ledger.DefaultIdentityData(privateKey)
 	store := common.NewInMemoryKVStore()
-	bootstrapSeqID, genesisRoot := InitStateStore(*id, store)
+	bootstrapSeqID, genesisRoot := multistate.InitStateStore(*id, store)
 
-	rootData := FetchAllRootRecords(store)
+	rootData := multistate.FetchAllRootRecords(store)
 	require.EqualValues(t, 1, len(rootData))
 
-	branchData := FetchBranchDataByRoot(store, rootData[0])
+	branchData := multistate.FetchBranchDataByRoot(store, rootData[0])
 	require.EqualValues(t, bootstrapSeqID, branchData.SequencerID)
 	require.True(t, ledger.CommitmentModel.EqualCommitments(genesisRoot, branchData.Root))
 
-	rdr := MustNewSugaredReadableState(store, genesisRoot)
+	rdr := multistate.MustNewSugaredReadableState(store, genesisRoot)
 
 	stemBack := rdr.GetStemOutput()
-	require.EqualValues(t, ledger.GenesisStemOutputID(id.GenesisSlot), stemBack.ID)
+	require.EqualValues(t, ledger.GenesisStemOutputID(), stemBack.ID)
 
 	initSupplyOut, err := rdr.GetChainOutput(&bootstrapSeqID)
 	require.NoError(t, err)
-	require.EqualValues(t, ledger.GenesisOutputID(id.GenesisSlot), initSupplyOut.ID)
+	require.EqualValues(t, ledger.GenesisOutputID(), initSupplyOut.ID)
 
 	require.EqualValues(t, id.Bytes(), rdr.MustLedgerIdentityBytes())
 }
