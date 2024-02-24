6857b60f51f0238cad85267859e8bd23858064b7
lunfardo314 <evaldas.drasutis@iota.org>
2024-02-24 18:12:35 +0200

    minor cleanup

--- core/attacher/types.go
+++ core/attacher/types.go
@@ -95,6 +95,7 @@ type (
 	}
 	Option func(*_attacherOptions)
 
+	// final values of attacher run. Ugly -> TODO refactor
 	attachFinals struct {
 		numInputs         int
 		numOutputs        int
@@ -129,4 +130,30 @@ const (
 
 func (f Flags) FlagsUp(fl Flags) bool {
 	return f&fl == fl
+}
+
+func OptionWithTransactionMetadata(metadata *txmetadata.TransactionMetadata) Option {
+	return func(options *_attacherOptions) {
+		options.metadata = metadata
+	}
+}
+
+func OptionWithAttachmentCallback(fun func(vid *vertex.WrappedTx, err error)) Option {
+	return func(options *_attacherOptions) {
+		options.attachmentCallback = fun
+	}
+}
+
+func OptionPullNonBranch(options *_attacherOptions) {
+	options.pullNonBranch = true
+}
+
+func OptionDoNotLoadBranch(options *_attacherOptions) {
+	options.doNotLoadBranch = true
+}
+
+func OptionInvokedBy(name string) Option {
+	return func(options *_attacherOptions) {
+		options.calledBy = name
+	}
 }
