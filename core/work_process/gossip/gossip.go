667b0ebb1b812f4dc54e8d11b6d08205466ca200
lunfardo314 <evaldas.drasutis@iota.org>
2024-03-03 19:43:48 +0200

    gossip bloom filter

--- core/work_process/gossip/gossip.go
+++ core/work_process/gossip/gossip.go
@@ -1,9 +1,12 @@
 package gossip
 
 import (
+	"time"
+
 	"github.com/libp2p/go-libp2p/core/peer"
 	"github.com/lunfardo314/proxima/core/txmetadata"
 	"github.com/lunfardo314/proxima/global"
+	"github.com/lunfardo314/proxima/ledger"
 	"github.com/lunfardo314/proxima/ledger/transaction"
 	"github.com/lunfardo314/proxima/util/queue"
 )
@@ -24,19 +27,26 @@ type (
 	Gossip struct {
 		*queue.Queue[*Input]
 		Environment
+		// bloom filter to avoid most of the repeating gossips
+		gossipedFilter    map[ledger.TransactionIDVeryShort4]time.Time
+		gossipedFilterTTL time.Duration
 	}
 )
 
 const (
-	Name           = "gossip"
-	TraceTag       = Name
-	chanBufferSize = 10
+	Name                   = "gossip"
+	TraceTag               = Name
+	chanBufferSize         = 10
+	gossipedFilterTTLSlots = 6
+	purgePeriod            = 5 * time.Second
 )
 
 func New(env Environment) *Gossip {
 	return &Gossip{
-		Queue:       queue.NewQueueWithBufferSize[*Input](Name, chanBufferSize, env.Log().Level(), nil),
-		Environment: env,
+		Queue:             queue.NewQueueWithBufferSize[*Input](Name, chanBufferSize, env.Log().Level(), nil),
+		Environment:       env,
+		gossipedFilter:    make(map[ledger.TransactionIDVeryShort4]time.Time),
+		gossipedFilterTTL: gossipedFilterTTLSlots * ledger.L().ID.SlotDuration(),
 	}
 }
 
@@ -46,16 +56,56 @@ func (d *Gossip) Start() {
 		d.MarkWorkProcessStopped(Name)
 	})
 	d.Queue.Start(d, d.Environment.Ctx())
+	go d.purgeLoop()
 }
 
 func (d *Gossip) Consume(inp *Input) {
+	if inp == nil {
+		// purge filter command
+		d.Tracef(TraceTag, "purge gossiped tx filter")
+		d.purgeFilter()
+		return
+	}
+	// check filter
+	vsID := inp.Tx.ID().VeryShortID4()
+	if _, already := d.gossipedFilter[vsID]; already {
+		// repeating, ignore
+		d.Tracef(TraceTag, "%s already gossiped, ignore", inp.Tx.IDShortString)
+		return
+	}
+	d.gossipedFilter[vsID] = time.Now().Add(d.gossipedFilterTTL)
+
 	if inp.ReceivedFrom == nil {
 		d.Tracef(TraceTag, "send %s to all peers, meta: %s",
-			inp.Tx.IDShortString, inp.Metadata.String())
+			inp.Tx.IDShortString, inp.Metadata.String)
 		d.GossipTxBytesToPeers(inp.Tx.Bytes(), &inp.Metadata)
 	} else {
 		d.Tracef(TraceTag, "send %s to peers except %s(%s), meta: %s",
-			inp.Tx.IDShortString, inp.ReceivedFrom, d.PeerName(*inp.ReceivedFrom), inp.Metadata.String())
+			inp.Tx.IDShortString, inp.ReceivedFrom, d.PeerName(*inp.ReceivedFrom), inp.Metadata.String)
 		d.GossipTxBytesToPeers(inp.Tx.Bytes(), &inp.Metadata, *inp.ReceivedFrom)
+	}
+}
+
+func (d *Gossip) purgeFilter() {
+	nowis := time.Now()
+	toDelete := make([]ledger.TransactionIDVeryShort4, 0)
+	for vsID, ttl := range d.gossipedFilter {
+		if ttl.Before(nowis) {
+			toDelete = append(toDelete, vsID)
+		}
+	}
+	for _, vsID := range toDelete {
+		delete(d.gossipedFilter, vsID)
+	}
+}
+
+func (d *Gossip) purgeLoop() {
+	for {
+		select {
+		case <-d.Ctx().Done():
+			return
+		case <-time.After(purgePeriod):
+			d.Push(nil, true)
+		}
 	}
 }
