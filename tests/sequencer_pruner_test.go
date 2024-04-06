aa9891101a64f39f8c53159c1bcd548b444ac48d
lunfardo314 <evaldas.drasutis@iota.org>
2024-04-06 18:50:41 +0300

    seq all tests pass

--- tests/sequencer_pruner_test.go
+++ tests/sequencer_pruner_test.go
@@ -256,7 +256,7 @@ func TestNSequencersTransferPruner(t *testing.T) {
 			nSequencers     = 2   // in addition to bootstrap
 			batchSize       = 10  // 10
 			sendAmount      = 2000
-			spammingTimeout = 20 * time.Second // 10
+			spammingTimeout = 60 * time.Second // 10
 			startPruner     = true
 			traceTx         = false
 		)
