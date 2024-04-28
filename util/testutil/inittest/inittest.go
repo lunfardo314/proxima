3644b5ca17e1e1f62e3efdbec7e252e560cb623c
lunfardo314 <evaldas.drasutis@iota.org>
2024-04-28 13:50:36 +0300

    cleanup

--- util/testutil/inittest/inittest.go
+++ util/testutil/inittest/inittest.go
@@ -8,23 +8,6 @@ import (
 	"github.com/lunfardo314/proxima/util/testutil"
 )
 
-const (
-	InitSupply = ledger.DefaultInitialSupply
-)
-
-func GenesisParamsWithPreDistributionOld(n int, initBalance uint64) ([]ledger.LockBalance, []ed25519.PrivateKey, []ledger.AddressED25519) {
-	privateKeys := testutil.GetTestingPrivateKeys(n)
-	addresses := ledger.AddressesED25519FromPrivateKeys(privateKeys)
-	distrib := make([]ledger.LockBalance, len(addresses))
-	for i := range addresses {
-		distrib[i] = ledger.LockBalance{
-			Lock:    addresses[i],
-			Balance: initBalance,
-		}
-	}
-	return distrib, privateKeys, addresses
-}
-
 const minimumAmountToDistribute = 1_000_000
 
 func GenesisParamsWithPreDistribution(initBalance ...uint64) ([]ledger.LockBalance, []ed25519.PrivateKey, []ledger.AddressED25519) {
