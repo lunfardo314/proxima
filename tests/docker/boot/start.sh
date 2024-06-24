d04c8a25fd48b54686d99af763189c45ed17aacf
HM4704 <hermann.moosburger@gmail.com>
2024-06-24 16:22:08 +0200

    rebasing

--- tests/docker/boot/start.sh
+++ tests/docker/boot/start.sh
@@ -5,25 +5,36 @@ INITIALIZED_FILE="/initialized"
 # Parameters
 SOURCE_PARAM=$1
 
-
-
 # Copy files from selected node directory to target directory
 cp -r ./"$SOURCE_PARAM"/*.yaml .
 
+kill_proxima() {
+    # Find the PID of proxima
+    pid=$(ps aux | grep proxima | grep -v grep | awk '{print $2}')
+
+    # Check if the PID variable is not empty
+    if [ -n "$pid" ]; then
+        echo "Killing proxima $pid"
+        kill $pid
+    else
+        echo "Proxima process not found"
+    fi
+}
+
 if [ ! -f "$INITIALIZED_FILE" ]; then
+    # node not initialized
     echo "image not initialized"
 
+    echo "init genesis_db"
     ./proxi init genesis_db
 
     if [ "$SOURCE_PARAM" == "boot" ]; then
+        # 
+        echo "init bootstrap_account"
         ./proxi init bootstrap_account
-    fi
-fi
 
-
-if [ ! -f "$INITIALIZED_FILE" ]; then
-    if [ "$SOURCE_PARAM" == "boot" ]; then
         ./proxima &
+
         sleep 10
         ./proxi -f node sequencer withdraw --finality.weak 800000000000000
         
@@ -39,7 +50,18 @@ if [ ! -f "$INITIALIZED_FILE" ]; then
             sleep 5
         done
     fi
-    
+    if [ "$SOURCE_PARAM" == "1" ] || [ "$SOURCE_PARAM" == "4" ]; then
+        echo "setup sequencer"
+
+        ./proxima &
+        sleep 5  # let process start
+
+        ./proxi node setup_seq --finality.weak mySeq 100000000000000
+
+        kill_proxima
+
+    fi 
+
     # Create the initialized file to mark the container as initialized
     touch "$INITIALIZED_FILE"
 fi
