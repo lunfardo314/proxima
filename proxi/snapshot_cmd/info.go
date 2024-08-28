8f5defb0c41b4533e25acc6e6ee62aa2beb8555b
lunfardo314 <evaldas.drasutis@iota.org>
2024-08-28 12:15:25 +0300

    proxi ux

--- proxi/snapshot_cmd/info.go
+++ proxi/snapshot_cmd/info.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"sort"
 
 	"github.com/lunfardo314/proxima/multistate"
 	"github.com/lunfardo314/proxima/proxi/glb"
+	"github.com/lunfardo314/proxima/util"
 	"github.com/lunfardo314/unitrie/common"
 	"github.com/spf13/cobra"
 )
@@ -16,7 +19,7 @@ func initSnapshotInfoCmd() *cobra.Command {
 	snapshotInfoCmd := &cobra.Command{
 		Use:   "info",
 		Short: "reads snapshot file and displays main info",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.MaximumNArgs(1),
 		Run:   runSnapshotInfoCmd,
 	}
 
@@ -25,11 +28,36 @@ func initSnapshotInfoCmd() *cobra.Command {
 }
 
 func runSnapshotInfoCmd(_ *cobra.Command, args []string) {
-	kvStream, err := multistate.OpenSnapshotFileStream(args[0])
+	var fname string
+	if len(args) == 0 {
+		entries, err := os.ReadDir("")
+		glb.AssertNoError(err)
+
+		entries = util.PurgeSlice(entries, func(entry os.DirEntry) bool {
+			ok, err := filepath.Match("*.snapshot", entry.Name())
+			if err == nil {
+				_, err = entry.Info()
+			}
+			return err == nil && ok
+		})
+
+		glb.Assertf(len(entries) > 0, "no snapshot files found")
+		sort.Slice(entries, func(i, j int) bool {
+			fii, _ := entries[i].Info()
+			fij, _ := entries[j].Info()
+			return fii.ModTime().After(fij.ModTime())
+		})
+		fname = entries[0].Name()
+	} else {
+		fname = args[0]
+	}
+
+	kvStream, err := multistate.OpenSnapshotFileStream(fname)
 	glb.AssertNoError(err)
 	defer kvStream.Close()
 
-	glb.Infof("snapshot file ok. Format version: %s", kvStream.Header.Version)
+	glb.Infof("snapshot file: %s", fname)
+	glb.Infof("format version: %s", kvStream.Header.Version)
 	glb.Infof("branch ID: %s", kvStream.BranchID.String())
 	glb.Infof("root record:\n%s", kvStream.RootRecord.Lines("    ").String())
 	glb.Infof("ledger id:\n%s", kvStream.LedgerID.Lines("    ").String())
