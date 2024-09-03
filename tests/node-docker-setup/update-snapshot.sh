bd14aae334b9a58468580b13a236278203ec7b7f
HM4704 <hermann.moosburger@gmail.com>
2024-09-03 19:21:46 +0200

    query for pwd

--- tests/node-docker-setup/update-snapshot.sh
+++ tests/node-docker-setup/update-snapshot.sh
@@ -5,6 +5,7 @@ REMOTE_USER="hd"                # Remote username
 REMOTE_HOST="185.139.228.149"   # Remote host IP or domain
 REMOTE_DIR="/home/hd"           # Remote directory to search
 LOCAL_SAVE_PATH="./data"  # Local path where the file will be saved
+PASSWORD=""        # Password for SSH (use with caution)
 
 # Check if local save directory exists
 if [ ! -d "$(dirname "$LOCAL_SAVE_PATH")" ]; then
@@ -12,12 +13,15 @@ if [ ! -d "$(dirname "$LOCAL_SAVE_PATH")" ]; then
     exit 1
 fi
 
+read -p "Enter the password: " PASSWORD
+
+
 if [ ! -d "${LOCAL_SAVE_PATH}" ]; then
     mkdir "${LOCAL_SAVE_PATH}"
 fi
 
 # Find the most recent file in the remote directory
-RECENT_FILE=$(ssh "${REMOTE_USER}@${REMOTE_HOST}" "ls -t ${REMOTE_DIR} | head -n 1")
+RECENT_FILE=$(sshpass -p "$PASSWORD" ssh "${REMOTE_USER}@${REMOTE_HOST}" "ls -t ${REMOTE_DIR} | head -n 1")
 
 if [ -z "$RECENT_FILE" ]; then
     echo "No files found in the remote directory."
@@ -30,7 +34,7 @@ REMOTE_FILE_PATH="${REMOTE_DIR}/${RECENT_FILE}"
 echo "Most recent file found: ${REMOTE_FILE_PATH}"
 
 # Download the most recent file
-scp "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_FILE_PATH}" "${LOCAL_SAVE_PATH}"
+sshpass -p "$PASSWORD" scp "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_FILE_PATH}" "${LOCAL_SAVE_PATH}"
 
 # Check if the scp command was successful
 if [ $? -eq 0 ]; then
