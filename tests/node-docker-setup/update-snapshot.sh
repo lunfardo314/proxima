f3acb15a9698e5ef28e36396c6b9f3745aacd897
HM4704 <hermann.moosburger@gmail.com>
2024-09-06 15:14:44 +0200

    some fixes

--- tests/node-docker-setup/update-snapshot.sh
+++ tests/node-docker-setup/update-snapshot.sh
@@ -5,12 +5,12 @@ URL="http://83.229.84.197/shared"
 
 # Fetch the directory listing, sort by modification date, and extract the most recent file
 #RECENT_FILE=$(curl -s $URL | grep -Eo 'href="[^"]+"' | sed 's/href="//g' | sed 's/"//g' | grep -v "/$" | grep -v "index.html" | sort | tail -n 1)
-RECENT_FILE=$(curl -s $URL | grep -Eo 'href="[^"]+"' | sed 's/href="//g' | sed 's/"//g' | grep -v "/$" | tail -n 1)
+RECENT_FILE=$(curl -L -s $URL | grep -Eo 'href="[^"]+"' | sed 's/href="//g' | sed 's/"//g' | grep -v "/$" | tail -n 1)
 
 # If a file is found, download it
 if [ -n "$RECENT_FILE" ]; then
     echo "Downloading the most recent file: $RECENT_FILE"
-    wget "${URL}/${RECENT_FILE}" -P ./data/${RECENT_FILE}
+    wget "${URL}/${RECENT_FILE}" -P ./data/
 else
     echo "No files found in the directory."
 fi
@@ -18,5 +18,5 @@ fi
 sudo rm -rf "${LOCAL_SAVE_PATH}/proximadb.txstore"
 sudo rm -rf "${LOCAL_SAVE_PATH}/proximadb"
 cd "${LOCAL_SAVE_PATH}"
-proxi snapshot restore "${RECENT_FILE}"
+proxi snapshot restore "./data/${RECENT_FILE}"
 cd ..
