8a3e969c6e5524821100a2e56ae5e89e9626d0be
lunfardo314 <evaldas.drasutis@iota.org>
2024-09-16 10:00:54 +0300

    prevent repeating snapshots

--- multistate/branches.go
+++ multistate/branches.go
@@ -98,6 +98,15 @@ func (r *RootRecord) Lines(prefix ...string) *lines.Lines {
 	return ret
 }
 
+func (br *BranchData) Lines(prefix ...string) *lines.Lines {
+	ret := br.RootRecord.Lines(prefix...)
+	ret.Add("Stem:").
+		Append(br.Stem.Lines("  ")).
+		Add("Sequencer output:").
+		Append(br.SequencerOutput.Lines("  "))
+	return ret
+}
+
 func (r *RootRecord) LinesVerbose(prefix ...string) *lines.Lines {
 	ret := r.Lines(prefix...)
 	ret.Add("root: %s", r.Root.String()).
