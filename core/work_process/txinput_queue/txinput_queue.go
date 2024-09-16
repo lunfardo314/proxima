f3f72a77f2b750296fe123cdf7619ab9c01bb636
lunfardo314 <evaldas.drasutis@iota.org>
2024-09-16 09:40:08 +0300

    input queue metrics

--- core/work_process/txinput_queue/txinput_queue.go
+++ core/work_process/txinput_queue/txinput_queue.go
@@ -10,9 +10,10 @@ import (
 	"github.com/lunfardo314/proxima/ledger"
 	"github.com/lunfardo314/proxima/ledger/transaction"
 	"github.com/lunfardo314/proxima/util"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-// transaction input queue to bugger incoming transactions from peers and from API
+// transaction input queue to buffer incoming transactions from peers and from API
 // Maintains bloom filter and check repeating transactions (with small probability of false positives)
 
 type (
@@ -36,6 +37,13 @@ type (
 		*work_process.WorkProcess[Input]
 		bloomFilter    map[ledger.TransactionIDVeryShort4]time.Time
 		bloomFilterTTL time.Duration
+		// metrics
+		inputTxCounter   prometheus.Counter
+		pulledTxCounter  prometheus.Counter
+		badTxCounter     prometheus.Counter
+		filterHitCounter prometheus.Counter
+		gossipedCounter  prometheus.Counter
+		queueSize        prometheus.Gauge
 	}
 )
 
@@ -64,10 +72,14 @@ func New(env environment) *TxInputQueue {
 		ret.Push(Input{Cmd: CmdPurge}, true)
 		return true
 	})
+
+	ret.registerMetrics()
 	return ret
 }
 
 func (q *TxInputQueue) consume(inp Input) {
+	q.inputTxCounter.Inc()
+
 	switch inp.Cmd {
 	case CmdFromPeer:
 		q.fromPeer(&inp)
@@ -83,6 +95,7 @@ func (q *TxInputQueue) consume(inp Input) {
 func (q *TxInputQueue) fromPeer(inp *Input) {
 	tx, err := transaction.FromBytes(inp.TxBytes)
 	if err != nil {
+		q.badTxCounter.Inc()
 		q.Log().Warn("TxInputQueue: %v", err)
 		return
 	}
@@ -94,8 +107,10 @@ func (q *TxInputQueue) fromPeer(inp *Input) {
 	if metaData.IsResponseToPull {
 		// do not check bloom filter if transaction was pulled
 		if err = q.TxInFromPeer(tx, metaData, inp.FromPeer); err != nil {
+			q.badTxCounter.Inc()
 			q.Log().Warn("TxInputQueue from peer %s: %v", inp.FromPeer.String(), err)
 		}
+		q.pulledTxCounter.Inc()
 		// yet put it into the bloom filter
 		q.bloomFilter[tx.ID().VeryShortID4()] = time.Now().Add(q.bloomFilterTTL)
 		return
@@ -103,6 +118,7 @@ func (q *TxInputQueue) fromPeer(inp *Input) {
 	// check bloom filter
 	if _, hit := q.bloomFilter[tx.ID().VeryShortID4()]; hit {
 		// filter hit, ignore transaction. May be rare false positive!!
+		q.filterHitCounter.Inc()
 		return
 	}
 
@@ -110,25 +126,30 @@ func (q *TxInputQueue) fromPeer(inp *Input) {
 	q.bloomFilter[tx.ID().VeryShortID4()] = time.Now().Add(q.bloomFilterTTL)
 
 	if err = q.TxInFromPeer(tx, metaData, inp.FromPeer); err != nil {
+		q.badTxCounter.Inc()
 		q.Log().Warn("TxInputQueue from peer %s: %v", inp.FromPeer.String(), err)
 		return
 	}
 	// gossiping all new pre-validated and not pulled transactions from peers
 	q.GossipTxBytesToPeers(inp.TxBytes, inp.TxMetaData, inp.FromPeer)
+	q.gossipedCounter.Inc()
 }
 
 func (q *TxInputQueue) fromAPI(inp *Input) {
 	tx, err := transaction.FromBytes(inp.TxBytes)
 	if err != nil {
+		q.badTxCounter.Inc()
 		q.Log().Warn("TxInputQueue from API: %v", err)
 		return
 	}
 	if err = q.TxInFromAPI(tx, inp.TraceFlag); err != nil {
+		q.badTxCounter.Inc()
 		q.Log().Warn("TxInputQueue from API: %v", err)
 		return
 	}
 	// gossiping all pre-validated transactions from API
-	q.GossipTxBytesToPeers(inp.TxBytes, inp.TxMetaData, inp.FromPeer)
+	q.GossipTxBytesToPeers(inp.TxBytes, inp.TxMetaData)
+	q.gossipedCounter.Inc()
 }
 
 func (q *TxInputQueue) purge() {
@@ -139,4 +160,33 @@ func (q *TxInputQueue) purge() {
 	for _, k := range toDelete {
 		delete(q.bloomFilter, k)
 	}
+}
+
+func (q *TxInputQueue) registerMetrics() {
+	q.inputTxCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "proxima_txInputQueue_in",
+		Help: "input queue counter",
+	})
+	q.pulledTxCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "proxima_txInputQueue_pulled",
+		Help: "number of pulled transactions",
+	})
+	q.badTxCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "proxima_txInputQueue_bad",
+		Help: "number of non-parseable transaction messages",
+	})
+	q.filterHitCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "proxima_txInputQueue_repeating",
+		Help: "number of bloom filter hit",
+	})
+	q.gossipedCounter = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "proxima_txInputQueue_gossiped",
+		Help: "number of gossiped",
+	})
+	q.queueSize = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "proxima_txInputQueue_queueSize",
+		Help: "size of the input queue",
+	})
+
+	q.MetricsRegistry().MustRegister(q.inputTxCounter, q.pulledTxCounter, q.badTxCounter, q.filterHitCounter, q.gossipedCounter, q.queueSize)
 }
