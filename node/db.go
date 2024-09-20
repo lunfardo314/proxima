b2d8b8ebdbd9962b9b2c0283592c5d7e3d6df7e8
lunfardo314 <evaldas.drasutis@iota.org>
2024-09-20 18:51:01 +0300

    Badger GC added

---
+++ node/db.go
@@ -0,0 +1,88 @@
+package node
+
+import (
+	"encoding/hex"
+	"time"
+
+	"github.com/lunfardo314/proxima/global"
+	"github.com/lunfardo314/proxima/ledger"
+	"github.com/lunfardo314/proxima/multistate"
+	"github.com/lunfardo314/proxima/txstore"
+	"github.com/lunfardo314/unitrie/adaptors/badger_adaptor"
+	"github.com/spf13/viper"
+)
+
+// initMultiStateLedger opens ledger state DB and initializes global ledger object
+func (p *ProximaNode) initMultiStateLedger() {
+	var err error
+	dbname := global.MultiStateDBName
+	bdb, err := badger_adaptor.OpenBadgerDB(dbname)
+	if err != nil {
+		p.Log().Fatalf("can't open '%s'", dbname)
+	}
+	p.dbClosedWG.Add(1)
+	p.multiStateDB = badger_adaptor.New(bdb)
+	p.Log().Infof("opened multi-state DB '%s'", dbname)
+
+	// initialize global ledger object with the ledger ID data from DB
+	multistate.InitLedgerFromStore(p.multiStateDB)
+	p.Log().Infof("ledger identity:\n%s", ledger.L().ID.Lines("       ").String())
+	h := ledger.L().LibraryHash()
+	p.Log().Infof("ledger constraint library hash: %s", hex.EncodeToString(h[:]))
+
+	p.RepeatInBackground("Badger_DB_GC_loop", 5*time.Minute, func() bool {
+		p.databaseGC()
+		return true
+	})
+
+	go func() {
+		// wait until others will stop
+		<-p.workProcessesStopStepChan
+		select {
+		case <-p.workProcessesStopStepChan:
+		case <-time.After(10 * time.Second):
+			p.Log().Warnf("forced close of multi-state DB")
+		}
+		_ = p.multiStateDB.Close()
+		p.Log().Infof("multi-state database has been closed")
+		p.dbClosedWG.Done()
+	}()
+}
+
+func (p *ProximaNode) initTxStore() {
+	switch viper.GetString(global.ConfigKeyTxStoreType) {
+	case "dummy":
+		p.Log().Infof("transaction store is 'dummy'")
+		p.txBytesStore = txstore.NewDummyTxBytesStore()
+
+	case "url":
+		panic("'url' type of transaction store is not supported yet")
+
+	default:
+		// default option is predefined database name
+		dbname := global.TxStoreDBName
+		p.Log().Infof("transaction store database dbname is '%s'", dbname)
+		p.txStoreDB = badger_adaptor.New(badger_adaptor.MustCreateOrOpenBadgerDB(dbname))
+		p.dbClosedWG.Add(1)
+		p.txBytesStore = txstore.NewSimpleTxBytesStore(p.txStoreDB, p)
+		p.Log().Infof("opened DB '%s' as transaction store", dbname)
+
+		go func() {
+			<-p.workProcessesStopStepChan
+			select {
+			case <-p.workProcessesStopStepChan:
+			case <-time.After(10 * time.Second):
+				p.Log().Warnf("forced close of transaction store DB")
+			}
+			_ = p.txStoreDB.Close()
+			p.Log().Infof("transaction store database has been closed")
+			p.dbClosedWG.Done()
+		}()
+	}
+}
+
+func (p *ProximaNode) databaseGC() {
+	start := time.Now()
+	err := p.txStoreDB.RunValueLogGC(0.5)
+	p.Log().Infof("----- Badger DB GC (%v): %v", time.Since(start), err)
+}
--- node/node.go
+++ node/node.go
@@ -1,7 +1,6 @@
 package node
 
 import (
-	"encoding/hex"
 	"fmt"
 	"net/http"
 	"runtime"
@@ -11,10 +10,8 @@ import (
 	"github.com/lunfardo314/proxima/core/workflow"
 	"github.com/lunfardo314/proxima/global"
 	"github.com/lunfardo314/proxima/ledger"
-	"github.com/lunfardo314/proxima/multistate"
 	"github.com/lunfardo314/proxima/peering"
 	"github.com/lunfardo314/proxima/sequencer"
-	"github.com/lunfardo314/proxima/txstore"
 	"github.com/lunfardo314/proxima/util"
 	"github.com/lunfardo314/unitrie/adaptors/badger_adaptor"
 	"github.com/prometheus/client_golang/prometheus"
@@ -141,70 +138,6 @@ func (p *ProximaNode) Start() {
 
 	p.goLoggingMemStats()
 	p.goLoggingSync()
-}
-
-// initMultiStateLedger opens ledger state DB and initializes global ledger object
-func (p *ProximaNode) initMultiStateLedger() {
-	var err error
-	dbname := global.MultiStateDBName
-	bdb, err := badger_adaptor.OpenBadgerDB(dbname)
-	if err != nil {
-		p.Log().Fatalf("can't open '%s'", dbname)
-	}
-	p.dbClosedWG.Add(1)
-	p.multiStateDB = badger_adaptor.New(bdb)
-	p.Log().Infof("opened multi-state DB '%s'", dbname)
-
-	// initialize global ledger object with the ledger ID data from DB
-	multistate.InitLedgerFromStore(p.multiStateDB)
-	p.Log().Infof("ledger identity:\n%s", ledger.L().ID.Lines("       ").String())
-	h := ledger.L().LibraryHash()
-	p.Log().Infof("ledger constraint library hash: %s", hex.EncodeToString(h[:]))
-
-	go func() {
-		// wait until others will stop
-		<-p.workProcessesStopStepChan
-		select {
-		case <-p.workProcessesStopStepChan:
-		case <-time.After(10 * time.Second):
-			p.Log().Warnf("forced close of multi-state DB")
-		}
-		_ = p.multiStateDB.Close()
-		p.Log().Infof("multi-state database has been closed")
-		p.dbClosedWG.Done()
-	}()
-}
-
-func (p *ProximaNode) initTxStore() {
-	switch viper.GetString(global.ConfigKeyTxStoreType) {
-	case "dummy":
-		p.Log().Infof("transaction store is 'dummy'")
-		p.txBytesStore = txstore.NewDummyTxBytesStore()
-
-	case "url":
-		panic("'url' type of transaction store is not supported yet")
-
-	default:
-		// default option is predefined database name
-		dbname := global.TxStoreDBName
-		p.Log().Infof("transaction store database dbname is '%s'", dbname)
-		p.txStoreDB = badger_adaptor.New(badger_adaptor.MustCreateOrOpenBadgerDB(dbname))
-		p.dbClosedWG.Add(1)
-		p.txBytesStore = txstore.NewSimpleTxBytesStore(p.txStoreDB, p)
-		p.Log().Infof("opened DB '%s' as transaction store", dbname)
-
-		go func() {
-			<-p.workProcessesStopStepChan
-			select {
-			case <-p.workProcessesStopStepChan:
-			case <-time.After(10 * time.Second):
-				p.Log().Warnf("forced close of transaction store DB")
-			}
-			_ = p.txStoreDB.Close()
-			p.Log().Infof("transaction store database has been closed")
-			p.dbClosedWG.Done()
-		}()
-	}
 }
 
 func (p *ProximaNode) initPeering() {
