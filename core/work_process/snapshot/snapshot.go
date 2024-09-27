ca3aad7faa689c2445fe27875a4f6bc6131da11a
lunfardo314 <evaldas.drasutis@iota.org>
2024-09-27 12:06:06 +0300

    minor

--- core/work_process/snapshot/snapshot.go
+++ core/work_process/snapshot/snapshot.go
@@ -84,7 +84,7 @@ func Start(env environment) {
 		return true
 	}, true)
 
-	ret.Log().Infof("[snapshot] work process STARTED\n        target directory: %s\n        period: %v (%d slots)\n        keep latest: %d",
+	ret.Log().Infof("[snapshot] work process STARTED\n        target directory: %s\n        frequency: %v (%d slots)\n        keep latest: %d",
 		ret.directory, period, periodInSlots, ret.keepLatest)
 	return
 }
