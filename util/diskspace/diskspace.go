6ea0c5bd36579bd807408e375ca7d2b3e6f2cb91
lunfardo314 <evaldas.drasutis@iota.org>
2024-10-07 09:37:35 +0300

    low disk space warning in logs

--- util/diskspace/diskspace.go
+++ util/diskspace/diskspace.go
@@ -2,33 +2,12 @@ package main
 
 import (
 	"fmt"
-	"syscall"
+
+	"github.com/lunfardo314/proxima/util"
 )
 
-func getDiskUsage(path string) (uint64, uint64, uint64) {
-	var stat syscall.Statfs_t
-
-	// Get file system statistics
-	err := syscall.Statfs(path, &stat)
-	if err != nil {
-		fmt.Println("Error getting disk usage:", err)
-		return 0, 0, 0
-	}
-
-	// Total available blocks * size per block = total space
-	total := stat.Blocks * uint64(stat.Bsize)
-
-	// Available blocks for non-superuser * size per block = free space available to normal users
-	available := stat.Bavail * uint64(stat.Bsize)
-
-	// Free blocks * size per block = total free space (including root user)
-	free := stat.Bfree * uint64(stat.Bsize)
-
-	return total, available, free
-}
-
 func main() {
-	total, available, free := getDiskUsage("/")
+	total, available, free := util.GetDiskUsage("/")
 	fmt.Printf("Total space: %.2f GB\n", float64(total)/(1024*1024*1024))
 	fmt.Printf("Available space: %.2f GB\n", float64(available)/(1024*1024*1024))
 	fmt.Printf("Free space: %.2f GB\n", float64(free)/(1024*1024*1024))
