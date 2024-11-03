e73390735848aaf0c162a0a10ee745c618c677c1
lunfardo314 <evaldas.drasutis@iota.org>
2024-11-03 16:07:09 +0200

    testnet candidate version

--- global/version.go
+++ global/version.go
@@ -6,7 +6,7 @@ import (
 
 // Version is the version of the Proxima node
 const (
-	Version        = "v0.1.2-alpha"
+	Version        = "v0.1.3-testnet"
 	bannerTemplate = "starting Proxima node version %s"
 )
 
