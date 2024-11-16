53b66849ea84734a4f6c7eee66ce0730da5f0496
lunfardo314 <evaldas.drasutis@iota.org>
2024-11-16 14:40:34 +0200

    wip

--- core/attacher/attacher_milestone.go
+++ core/attacher/attacher_milestone.go
@@ -101,10 +101,10 @@ func newMilestoneAttacher(vid *vertex.WrappedTx, env Environment, metadata *txme
 
 func (a *milestoneAttacher) run() error {
 	// first solidify baseline state
-	//if a.vid.IDHasFragment("[125419|") {
-	//	a.Log().Infof(">>>>>>>> inside run attacher %s", a.vid.IDShortString())
-	//	a.SetTraceAttacher(fmt.Sprintf("@@@>>2 ((%s))", a.vid.IDShortString()))
-	//}
+	if a.vid.IDHasFragment("[125417|0008e9e5") {
+		a.Log().Infof(">>>>>>>> inside run attacher %s", a.vid.IDShortString())
+		a.SetTraceAttacher("@@@>>")
+	}
 
 	if status := a.solidifyBaseline(); status != vertex.Good {
 		a.Tracef(TraceTagAttachMilestone, "baseline solidification failed. Reason: %v", a.err)
