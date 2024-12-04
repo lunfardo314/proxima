308663dfaa99f661b1dd80acc6134d479b83c696
lunfardo314 <evaldas.drasutis@iota.org>
2024-12-04 12:47:38 +0200

    proxi node last_seq

--- proxi/node_cmd/last_seq.go
+++ proxi/node_cmd/last_seq.go
@@ -27,6 +27,10 @@ func runLastMilestonesCmd(_ *cobra.Command, _ []string) {
 	lastSeq, err := glb.GetClient().GetLastKnownSequencerData()
 	glb.AssertNoError(err)
 
+	glb.Infof("    Sequencer ID                                                          " +
+		"TxID                                                                   Count" +
+		"  Seen sec ago" + "   Last branchID")
+
 	for _, seqID := range util.KeysSorted(lastSeq, util.StringsLess) {
 		chainID, err := ledger.ChainIDFromHexString(seqID)
 		glb.AssertNoError(err)
@@ -34,15 +38,15 @@ func runLastMilestonesCmd(_ *cobra.Command, _ []string) {
 		sd := lastSeq[seqID]
 		txid, err := ledger.TransactionIDFromHexString(sd.LatestMilestoneTxID)
 		glb.AssertNoError(err)
-		activity := time.Since(time.Unix(0, sd.LastActivityUnixNano))
+		activity := time.Since(time.Unix(0, sd.LastActivityUnixNano)) / time.Second
 
 		var branchID ledger.TransactionID
 		if sd.LastBranchTxID != "" {
 			branchID, err = ledger.TransactionIDFromHexString(sd.LastBranchTxID)
 			glb.AssertNoError(err)
-			glb.Infof("    %s :    %s   %5d  %v   %s", chainID.String(), txid.String(), sd.MilestoneCount, activity, branchID.StringShort())
+			glb.Infof("    %s    %s   %5d    %5d          %s", chainID.String(), txid.String(), sd.MilestoneCount, activity, branchID.StringShort())
 		} else {
-			glb.Infof("    %s :    %s   %5d  %v", chainID.String(), txid.String(), sd.MilestoneCount, activity)
+			glb.Infof("    %s    %s   %5d  %5d", chainID.String(), txid.String(), sd.MilestoneCount, activity)
 		}
 	}
 }
