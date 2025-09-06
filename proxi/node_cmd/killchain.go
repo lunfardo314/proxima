d3a20c17be305eb1208c282d1ae220f0dd59fc09
lunfardo314 <evaldas.drasutis@iota.org>
2025-09-06 11:52:31 +0300

    wip fixing killchain

--- proxi/node_cmd/killchain.go
+++ proxi/node_cmd/killchain.go
@@ -1,20 +1,15 @@
 package node_cmd
 
 import (
-	"context"
-	"crypto/ed25519"
-	"errors"
 	"fmt"
 	"os"
-	"sync"
 	"time"
 
 	"github.com/lunfardo314/proxima/ledger"
 	"github.com/lunfardo314/proxima/ledger/base"
-	"github.com/lunfardo314/proxima/ledger/multistate"
+	"github.com/lunfardo314/proxima/ledger/transaction"
 	"github.com/lunfardo314/proxima/ledger/txbuilder"
 	"github.com/lunfardo314/proxima/proxi/glb"
-	"github.com/lunfardo314/proxima/util/set"
 	"github.com/spf13/cobra"
 )
 
@@ -55,128 +50,47 @@ func runKillChainCmd(_ *cobra.Command, args []string) {
 	if md.MinimumFee() > feeAmount {
 		feeAmount = md.MinimumFee()
 	}
+	glb.Assertf(feeAmount > 0, "tag-along fee must be > 0")
 
 	prompt := fmt.Sprintf("discontinue chain %s?", chainID.String())
 	if !glb.YesNoPrompt(prompt, true, glb.BypassYesNoPrompt()) {
 		glb.Infof("exit")
 		os.Exit(0)
 	}
-	var wg sync.WaitGroup
-	ctx, cancel := context.WithCancel(context.Background())
 
-	wg.Add(2)
-	go func() {
-		checkChainLoop(chainID, 2*time.Second, ctx)
-		cancel()
-		wg.Done()
-	}()
+	out, _, _, err := clnt.GetChainOutput(chainID)
+	glb.AssertNoError(err)
 
-	time.Sleep(500 * time.Millisecond)
+	ts := ledger.TimeNow()
+	if ts.IsSlotBoundary() {
+		ts = ts.AddTicks(10)
+	}
+	dOut, isDelegation := ledger.AsDelegationOutput(out.Output, out.ID)
 
-	go func() {
-		makeTransactionLoop(killChainParams{
-			chainID:       chainID,
-			privateKey:    walletData.PrivateKey,
-			tagAlongSeqID: tagAlongSeqID,
-			tagAlongFee:   feeAmount,
-			repeatPeriod:  2 * time.Second,
-			ctx:           ctx,
+	var tx *transaction.Transaction
+	if !isDelegation {
+		tx, err = txbuilder.MakeEndChainTransaction(txbuilder.EndChainParams{
+			Timestamp:     ts,
+			ChainIn:       out,
+			PrivateKey:    walletData.PrivateKey,
+			TagAlongSeqID: *pTagAlongSeqID,
+			TagAlongFee:   feeAmount,
 		})
-		cancel()
-		wg.Done()
-	}()
-	wg.Wait()
-}
+	} else {
+		dOut.UnfreezeSlot()
+		IsInSafeRevocationWindow(uint32(ts.Slot))
 
-type killChainParams struct {
-	chainID       base.ChainID
-	privateKey    ed25519.PrivateKey
-	tagAlongSeqID base.ChainID
-	tagAlongFee   uint64
-	repeatPeriod  time.Duration
-	ctx           context.Context
-}
-
-// checkChainLoop polls chain in the LRB state and exits when chain does not exist anymore
-func checkChainLoop(chainID base.ChainID, repeatPeriod time.Duration, ctx context.Context) {
-	clnt := glb.GetClient()
-
-	for {
-		select {
-		case <-ctx.Done():
-			return
-		default:
-		}
-
-		_, _, _, err := clnt.GetChainOutput(chainID)
-		if errors.Is(err, multistate.ErrNotFound) {
-			glb.Infof("chain %s does not exist anymore", chainID.String())
-			return
-		}
-		glb.AssertNoError(err)
-		select {
-		case <-ctx.Done():
-			return
-		case <-time.After(repeatPeriod):
-		}
+		dconst := ledger.DelegationConst()
+		dconst.LastSlotInEpochDirect(dOut.Target.ChainID())
 
 	}
-}
 
-// makeTransactionLoop periodically issues new killchain transaction for each new LRB which has new delegation output
-// the transaction's timestamp is at the nearest liquidity window timestamp.
-// Multiple transactions are issued until one succeeds. The rest are double-spends and are orphaned
-func makeTransactionLoop(par killChainParams) {
-	clnt := glb.GetClient()
-	consumedOutputs := set.New[base.OutputID]()
-
-	attempt := 1
-	for {
-		o, _, lrbid, err := clnt.GetChainOutput(par.chainID)
-		if errors.Is(err, multistate.ErrNotFound) {
-			_, lrbid, err = clnt.GetLatestReliableBranch()
-			glb.AssertNoError(err)
-			glb.Infof("chain %s not found. LRB (latest reliable branch) is %s (%d slots behind from now)",
-				par.chainID.StringShort(), lrbid.String(), ledger.TimeNow().Slot-lrbid.Slot())
-			return
-		}
-		glb.AssertNoError(err)
-
-		if ledger.TimeNow().Slot-lrbid.Slot() > 2 {
-			glb.Infof("warning: LRB  (latest reliable branch) is %d slots behind from now. Node may not be synced", ledger.TimeNow().Slot-lrbid.Slot())
-		}
-		if !consumedOutputs.Contains(o.ID) {
-			ts := ledger.NextClosedDelegationTimestamp(par.chainID, o.Timestamp())
-
-			tx, err := txbuilder.MakeEndChainTransaction(txbuilder.EndChainParams{
-				Timestamp:     ts,
-				ChainIn:       o,
-				PrivateKey:    par.privateKey,
-				TagAlongSeqID: par.tagAlongSeqID,
-				TagAlongFee:   par.tagAlongFee,
-			})
-			glb.AssertNoError(err)
-
-			err = clnt.SubmitTransaction(tx.Bytes())
-			glb.AssertNoError(err)
-
-			err = clnt.SubmitTransaction(tx.Bytes())
-			glb.AssertNoError(err)
-
-			lrbBehindTicks := base.DiffTicks(lrbid.Timestamp(), ledger.TimeNow())
-			glb.Infof("attempt #%d. Submitted transaction %s. LRB (latest reliable branch) is %d ticks, %d slots, %v behind. ",
-				attempt, tx.IDString(), lrbBehindTicks, lrbBehindTicks/256, time.Duration(lrbBehindTicks)*ledger.TickDuration())
-			glb.Verbosef("-------------- transaction --------------\n%s", tx.String())
-
-			consumedOutputs.Insert(o.ID)
-			attempt++
-		}
-
-		select {
-		case <-par.ctx.Done():
-			return
-		case <-time.After(par.repeatPeriod):
-		}
-	}
+	tx, err := txbuilder.MakeEndChainTransaction(txbuilder.EndChainParams{
+		Timestamp:     nowis,
+		ChainIn:       out,
+		PrivateKey:    walletData.PrivateKey,
+		TagAlongSeqID: *pTagAlongSeqID,
+		TagAlongFee:   feeAmount,
+	})
 
 }
