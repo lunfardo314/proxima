148ab3e820c0e82ac8b897b473a81fd1db7f137e
lunfardo314 <evaldas.drasutis@iota.org>
2025-09-28 17:39:19 +0300

    trace restore

--- core/attacher/attach.go
+++ core/attacher/attach.go
@@ -22,12 +22,8 @@ func AttachTxID(txid base.TransactionID, env Environment, opts ...AttachTxOption
 
 	{
 		snapID := env.Branches().SnapshotBranchID()
-		if txid.IsBranchTransaction() {
-			env.Tracef("nearSnapshot", ">>>> AttachTxID branch: %s, hex = %s", txid.String(), txid.StringHex())
-		} else {
-			if txid.Slot() <= snapID.Slot()+1 {
-				env.Tracef("nearSnapshot", ">>>> AttachTxID(snapSlot = %d): %s   %s", snapID.Slot(), txid.String(), txid.StringHex())
-			}
+		if txid.Slot() <= snapID.Slot()+1 {
+			env.Tracef("nearSnapshot", ">>>> AttachTxID(snapSlot = %d): %s   %s", snapID.Slot(), txid.String(), txid.StringHex())
 		}
 	}
 
