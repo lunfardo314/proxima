29945a7ba0f4d151bc1b5f22e9de8b8f40beece8
lunfardo314 <evaldas.drasutis@iota.org>
2025-12-26 18:38:56 +0200

    proxi node get_inactive

--- proxi/node_cmd/get_inactive.go
+++ proxi/node_cmd/get_inactive.go
@@ -8,6 +8,7 @@ import (
 	"github.com/lunfardo314/proxima/ledger"
 	"github.com/lunfardo314/proxima/ledger/base"
 	"github.com/lunfardo314/proxima/proxi/glb"
+	"github.com/lunfardo314/proxima/util"
 	"github.com/spf13/cobra"
 )
 
@@ -41,19 +42,46 @@ func runGetInactiveCmd(_ *cobra.Command, args []string) {
 	type oBin struct {
 		oid     base.OutputID
 		lockStr string
+		amount  uint64
+		utxoStr string
 	}
 
 	outs1 := make([]*oBin, 0)
 	for _, o := range outs.UTXOs {
 		id, err := base.OutputIDFromHexString(o.ID)
 		glb.AssertNoError(err)
-		outs1 = append(outs1, &oBin{oid: id, lockStr: o.Lock})
+		outs1 = append(outs1, &oBin{
+			oid:     id,
+			lockStr: o.Lock,
+			amount:  o.Amount,
+			utxoStr: o.OutputString,
+		})
 	}
 	sort.Slice(outs1, func(i, j int) bool {
 		return bytes.Compare(outs1[i].oid[:], outs1[j].oid[:]) < 0
 	})
+	total := uint64(0)
 	for _, o := range outs1 {
-		glb.Infof("%75s: %s", o.oid.String(), o.lockStr)
+		glb.Infof("%75s: %s      %15s", o.oid.String(), o.lockStr, util.Th(o.amount))
+		glb.Verbosef("%s\n", o.utxoStr)
+		total += o.amount
 	}
+	glb.Infof("----------\ntotal inactive: %s", util.Th(total))
 
+	type addrTotal struct {
+		utxos int
+		total uint64
+	}
+	byAddr := make(map[string]addrTotal)
+
+	for _, o := range outs1 {
+		r := byAddr[o.lockStr]
+		r.utxos += 1
+		r.total += o.amount
+		byAddr[o.lockStr] = r
+	}
+	glb.Infof("------------\nby address:")
+	for addr, t := range byAddr {
+		glb.Infof("%25s: utxos: %d, total: %s", addr, t.utxos, util.Th(t.total))
+	}
 }
