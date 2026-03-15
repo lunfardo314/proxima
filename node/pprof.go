09dfe099a26cf2f9ca3f7fdd7852d9a363833319
lunfardo314 <evaldas.alt@gmail.com>
2026-03-15 10:50:16 +0200

    enable mutex and block profiling when pprof is active

--- node/pprof.go
+++ node/pprof.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	_ "net/http/pprof"
+	"runtime"
 
 	"github.com/lunfardo314/proxima/util"
 	"github.com/spf13/viper"
@@ -15,6 +16,10 @@ func (p *ProximaNode) startPProfIfEnabled() {
 	if !viper.GetBool("pprof.enable") {
 		return
 	}
+	// Enable mutex and block profiling for contention analysis
+	runtime.SetMutexProfileFraction(5)
+	runtime.SetBlockProfileRate(1000) // nanoseconds; captures blocks >= 1µs
+
 	port := viper.GetInt("pprof.port")
 	if port == 0 {
 		port = defaultPprofPort
