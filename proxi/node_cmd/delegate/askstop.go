eb40121c6cfd1512dc0b487b3bf55d17a4faf6d2
lunfardo314 <evaldas.alt@gmail.com>
2026-05-20 15:01:14 +0300

    proxi/delegate askstop: refactor to txbuildercore + helpers
    Phase 1.9 of the wasm-style refactor: build the ask-stop-delegation
    sequencer-request output via lib.NewSequencerRequestOutput +
    lib.NewEnsureStopDelegationConstraint. Wallet inputs consumed
    inline; submit via glb.SubmitAndDisplay.

--- proxi/node_cmd/delegate/askstop.go
+++ proxi/node_cmd/delegate/askstop.go
@@ -2,13 +2,16 @@ package delegate
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/lunfardo314/proxima/ledger"
 	"github.com/lunfardo314/proxima/ledger/base"
+	"github.com/lunfardo314/proxima/ledger/txbuildercore"
 	"github.com/lunfardo314/proxima/proxi/glb"
 	"github.com/lunfardo314/proxima/sequencer/txbuilder_seq"
 	"github.com/lunfardo314/proxima/util"
+	"github.com/lunfardo314/proxima/util/smallkv"
 	"github.com/spf13/cobra"
 )
 
@@ -60,25 +63,72 @@ func runRevokeDelegationCmd(_ *cobra.Command, args []string) {
 
 	compensation := dOut.RevocationCompensationEstimate(ts.Slot)
 	const minimumFee = 50
-
 	glb.Assertf(compensation >= minimumFee, "estimated compensation is even less than minimum fee %d", minimumFee)
 
-	requestOutput := txbuilder_seq.NewAskStopDelegationReqOutput(targetID, walletData.Account, delegationID, compensation)
+	// Wasm-style build via txbuildercore + helpers.
+	lib := glb.GetTxLibrary()
+	walletHolderID := base.HolderID(walletData.Account)
 
-	txBytes, txid, txString, err := glb.MakeSendOutputTransaction(requestOutput, walletData.PrivateKey, ts)
-	if err != nil {
-		glb.Infof("error: %v", err)
-		if txString != "" {
-			glb.Infof("------------ failing tx --------------\n" + txString)
+	// Pull wallet inputs (all sigLock-controlled outputs).
+	walletOutputs, _, amountInWallet, err := clnt.GetTransferableOutputs(walletData.Account, 255)
+	glb.AssertNoError(err)
+	glb.Assertf(len(walletOutputs) > 0, "wallet has no outputs to create transaction")
+	glb.Assertf(amountInWallet >= compensation, "not enough balance: have %d, need %d", amountInWallet, compensation)
+
+	txb := txbuildercore.New(0)
+	consumedBytes := make([][]byte, 0, len(walletOutputs))
+	for i, in := range walletOutputs {
+		b := in.Output.Bytes()
+		txb.ConsumeOutput(b, in.ID)
+		consumedBytes = append(consumedBytes, b)
+		if i == 0 {
+			txb.PutSignatureUnlock(0)
+		} else {
+			err := txb.PutUnlockReference(byte(i), ledger.ConstraintIndexLock, 0)
+			glb.AssertNoError(err)
 		}
-		return
 	}
+
+	// Compose the ask-stop-delegation sequencer-request output.
+	extra, err := lib.NewEnsureStopDelegationConstraint(delegationID)
+	glb.AssertNoError(err)
+	params := smallkv.New()
+	params.Set(txbuilder_seq.FieldRevokeDelegationID, delegationID[:])
+	reqOut, err := lib.NewSequencerRequestOutput(
+		compensation,
+		targetID,
+		walletHolderID,
+		txbuilder_seq.RequestCodeAskStopDelegation,
+		&params,
+		extra,
+	)
+	glb.AssertNoError(err)
+	txb.ProduceOutput(reqOut.Bytes())
+
+	// Remainder back to wallet.
+	if amountInWallet > compensation {
+		remainderOut, err := txbuildercore.NewSigLockOutput(lib, amountInWallet-compensation, walletHolderID)
+		glb.AssertNoError(err)
+		txb.ProduceOutput(remainderOut.Bytes())
+	}
+
+	txb.SetTimestamp(ts)
+	txb.ComputeInputCommitment()
+	txb.SignED25519(walletData.PrivateKey)
+
+	txBytes := txb.Bytes()
+	txid, err := txbuildercore.TxIDFromBytes(txBytes)
+	glb.AssertNoError(err)
+
 	prompt := fmt.Sprintf("send request to stop delegation %s to the sequencer %s?", delegationID.StringShort(), targetID.String())
 	if !glb.YesNoPrompt(prompt, true) {
-		return
+		glb.Infof("exit")
+		os.Exit(0)
 	}
-	err = clnt.SubmitTransaction(txBytes)
-	glb.AssertNoError(err)
+
+	if err := glb.SubmitAndDisplay(txBytes, consumedBytes...); err != nil {
+		os.Exit(1)
+	}
 
 	glb.TrackTxInclusion(txid, time.Second)
 }
