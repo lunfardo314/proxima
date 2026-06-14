lunfardo314/proxima

commit bf4477f5e70f491f96662aac43320aea2593fcd7

fix memory leak and GC

core/attacher/attacher_milestone.go
@@ -15,7 +15,6 @@ import (
 	"github.com/lunfardo314/proxima/ledger"
 	"github.com/lunfardo314/proxima/util"
 	"github.com/lunfardo314/proxima/util/checkpoints"
-	"github.com/lunfardo314/proxima/util/trackgc"
 )
 
 const (
@@ -23,9 +22,9 @@ const (
 	periodicCheckEach       = 50 * time.Millisecond
 )
 
-var trackedMilestoneAttachers = trackgc.New[milestoneAttacher](func(p *milestoneAttacher) string {
-	return "milestoneAttacher " + p.name
-})
+//var trackedMilestoneAttachers = trackgc.New[milestoneAttacher](func(p *milestoneAttacher) string {
+//	return "milestoneAttacher " + p.name
+//})
 
 func runMilestoneAttacher(
 	vid *vertex.WrappedTx,
@@ -143,35 +142,46 @@ func (a *milestoneAttacher) run() error {
 	// finalizing touches
 	a.wrapUpAttacher()
 
+	a.pastCone.SetFlagsUp(a.vid, vertex.FlagPastConeVertexDefined)
 	if a.vid.IsBranchTransaction() {
 		// branch transaction vertex is immediately detached. Thus branch transaction does not reference past cone
 		a.vid.ConvertToDetached()
-		a.vid.SetTxStatusGood(nil, a.pastCone.LedgerCoverage())
+		//a.vid.SetTxStatusGood(nil, a.pastCone.LedgerCoverage())
+		a.vid.SetTxStatusGood(a.pastCone.PastConeBase.CloneImmutable(), a.pastCone.LedgerCoverage())
 	} else {
 		a.vid.SetTxStatusGood(a.pastCone.PastConeBase.CloneImmutable(), a.pastCone.LedgerCoverage())
 		a.EvidencePastConeSize(a.pastCone.PastConeBase.Len())
 	}
+	//else {
+	//	a.vid.SetTxStatusGood(a.pastCone.PastConeBase.CloneImmutable(), a.pastCone.LedgerCoverage())
+	//	a.EvidencePastConeSize(a.pastCone.PastConeBase.Len())
+	//}
 
-	a.pastCone.SetFlagsUp(a.vid, vertex.FlagPastConeVertexDefined)
+	//if a.vid.IsBranchTransaction() {
+	//	a.vid.SetTxStatusGood(nil, a.pastCone.LedgerCoverage())
+	//} else {
+	//	a.vid.SetTxStatusGood(a.pastCone.PastConeBase.CloneImmutable(), a.pastCone.LedgerCoverage())
+	//}
+	//a.EvidencePastConeSize(a.pastCone.PastConeBase.Len())
 
-	{ // debug
-		const (
-			lastCheck     = false
-			printPastCone = false
-		)
-		if lastCheck {
-			err = a.pastCone.CheckFinalPastCone(a.baselineStateReader)
-			if err != nil {
-				err = fmt.Errorf("%w\n------ past cone of %s ------\n%s",
-					err, a.vid.IDShortString(), a.pastCone.Lines("     ").Join("\n"))
-				memdag.SaveGraphPastCone(a.vid, "past_cone_CheckFinalPastCone")
-			}
-			a.AssertNoError(err)
-		}
-		if printPastCone {
-			a.Log().Infof(">>>>>>>>>>>>> past cone of attacher %s\n%s", a.Name(), a.pastCone.Lines("      ").String())
-		}
-	}
+	//{ // debug
+	//	const (
+	//		lastCheck     = false
+	//		printPastCone = false
+	//	)
+	//	if lastCheck {
+	//		err = a.pastCone.CheckFinalPastCone(a.baselineStateReader)
+	//		if err != nil {
+	//			err = fmt.Errorf("%w\n------ past cone of %s ------\n%s",
+	//				err, a.vid.IDShortString(), a.pastCone.Lines("     ").Join("\n"))
+	//			memdag.SaveGraphPastCone(a.vid, "past_cone_CheckFinalPastCone")
+	//		}
+	//		a.AssertNoError(err)
+	//	}
+	//	if printPastCone {
+	//		a.Log().Infof(">>>>>>>>>>>>> past cone of attacher %s\n%s", a.Name(), a.pastCone.Lines("      ").String())
+	//	}
+	//}
 
 	a.SendToTippool(a.vid)
 

core/vertex/vid.go
@@ -83,19 +83,25 @@ func (vid *WrappedTx) ConvertVirtualTxToVertexNoLock(v *Vertex) {
 }
 
 // ConvertToDetached detaches past cone and leaves only a collection of produced outputs
+// Detaches input dependencies and converts to the DetachedVertex
+// Note, however, that for branches, WrappedTx with DetachedVertex can later contain reference to the pastCone structure.
+// Upon repeated calls to ConvertToDetached we set pastCone to nil. If not this, DAG remains always connected and
+// old vertices are not garbage collected -> memory leak!
 func (vid *WrappedTx) ConvertToDetached() {
 	vid.Unwrap(UnwrapOptions{
 		Vertex: func(v *Vertex) {
 			vid.convertToDetachedTxUnlocked(v)
-			fmt.Printf(">>>>>>> ConvertToDetached Vertex %s\n", vid.IDShortString())
+			vid.pastCone = nil
+			//fmt.Printf(">>>>>>> ConvertToDetached Vertex %s\n", vid.IDShortString())
 		},
 		DetachedVertex: func(v *DetachedVertex) {
-			vid.pastCone = nil // TODO temporary solution for the GC problem
-			fmt.Printf(">>>>>>> ConvertToDetached DetachedVertex %s\n", vid.IDShortString())
+			util.Assertf(vid.pastCone == nil || vid.IsBranchTransaction(), "vid.pastCone == nil ||vid.IsBranchTransaction()")
+			vid.pastCone = nil // Important: if not this, memdag leaks memory
+			//fmt.Printf(">>>>>>> ConvertToDetached DetachedVertex %s\n", vid.IDShortString())
 		},
 		VirtualTx: func(v *VirtualTransaction) {
 			util.Assertf(vid.pastCone == nil, "vid.pastCone == nil")
-			fmt.Printf(">>>>>>> ConvertToDetached VirtualTx %s\n", vid.IDShortString())
+			//fmt.Printf(">>>>>>> ConvertToDetached VirtualTx %s\n", vid.IDShortString())
 		},
 	})
 }
@@ -103,10 +109,10 @@ func (vid *WrappedTx) ConvertToDetached() {
 func (vid *WrappedTx) convertToDetachedTxUnlocked(v *Vertex) {
 	vid._put(_detachedVertex{v.toDetachedVertex()})
 	v.UnReferenceDependencies()
-	vid.pastCone.Dispose()
-	vid.pastCone = nil
 	vid.OnPokeNop()
 	vid.SetFlagsUpNoLock(FlagVertexIgnoreAbsenceOfPastCone)
+	//vid.pastCone.Dispose()
+	//vid.pastCone = nil
 	//vid.consumed = nil
 }
 
@@ -146,20 +152,6 @@ func (vid *WrappedTx) SetTxStatusGood(pastCone *PastConeBase, coverage uint64) {
 	if pastCone == nil {
 		vid.flags.SetFlagsUp(FlagVertexIgnoreAbsenceOfPastCone)
 	} else {
-		//if vid.IsBranchTransaction() && vid.Slot() == 10
-		{ // debug
-			deepestStr := "<nil>"
-			deepest := pastCone.DeepestReference(set.New[*WrappedTx]())
-			if deepest != nil {
-				deepestStr = deepest.IDShortString()
-			}
-			fmt.Printf(">>>>>>>> SetTxStatusGood in %s. Oldest ref: %s\n", vid.IDShortString(), deepestStr)
-
-			//if oldest.Slot() > 10 {
-			//	fmt.Printf("pastCone of %s: %s\n", )
-			//}
-
-		}
 		vid.pastCone = pastCone
 		if coverage > 0 {
 			vid.coverage = util.Ref(coverage)

core/attacher/attacher_incremental.go
@@ -10,14 +10,13 @@ import (
 	"github.com/lunfardo314/proxima/ledger/txbuilder"
 	"github.com/lunfardo314/proxima/util"
 	"github.com/lunfardo314/proxima/util/lines"
-	"github.com/lunfardo314/proxima/util/trackgc"
 )
 
 const TraceTagIncrementalAttacher = "incAttach"
 
-var trackedIncrementalAttachers = trackgc.New[IncrementalAttacher](func(p *IncrementalAttacher) string {
-	return "incAttacher " + p.name
-})
+//var trackedIncrementalAttachers = trackgc.New[IncrementalAttacher](func(p *IncrementalAttacher) string {
+//	return "incAttacher " + p.name
+//})
 
 func NewIncrementalAttacher(name string, env Environment, targetTs ledger.Time, extend vertex.WrappedOutput, endorse ...*vertex.WrappedTx) (*IncrementalAttacher, error) {
 	env.Assertf(ledger.ValidSequencerPace(extend.Timestamp(), targetTs), "NewIncrementalAttacher: target is closer than allowed pace (%d): %s -> %s",

core/memdag/memdag.go
@@ -12,7 +12,6 @@ import (
 	"github.com/lunfardo314/proxima/ledger"
 	"github.com/lunfardo314/proxima/ledger/multistate"
 	"github.com/lunfardo314/proxima/util"
-	"github.com/lunfardo314/proxima/util/trackgc"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/exp/maps"
 )
@@ -108,9 +107,9 @@ func init() {
 	util.Assertf(vertexTTLSlots >= _vertexTTLSlotsMinimum, "constant vertexTTLSlots must be at least %d", _vertexTTLSlotsMinimum)
 }
 
-var TrackedVertices = trackgc.New[vertex.WrappedTx](func(p *vertex.WrappedTx) string {
-	return p.IDShortString()
-})
+//var TrackedVertices = trackgc.New[vertex.WrappedTx](func(p *vertex.WrappedTx) string {
+//	return p.IDShortString()
+//})
 
 func (d *MemDAG) WithGlobalWriteLock(fun func()) {
 	d.mutex.Lock()
@@ -177,16 +176,16 @@ func (d *MemDAG) doGC() (detached, deleted int) {
 	expired = util.PurgeSlice(expired, func(vid *vertex.WrappedTx) bool {
 		if vid.NumReferences() == 0 {
 			vid.ConvertToDetached()
-			{ // debug
-				TrackedVertices.TrackPointerNotGCed(vid,
-					trackgc.WithTimeout(20*time.Second),
-					trackgc.WithPanicOnTimeout(false),
-					trackgc.WithReportTimeout(false),
-				)
-				//if vid.Slot() <= 1 {
-				//	TrackedVertices.TrackPointerNotGCed(vid, vid.IDShortString(), 5*time.Second)
-				//}
-			}
+			//{ // debug
+			//	TrackedVertices.TrackPointerNotGCed(vid,
+			//		trackgc.WithTimeout(20*time.Second),
+			//		trackgc.WithPanicOnTimeout(false),
+			//		trackgc.WithReportTimeout(false),
+			//	)
+			//	//if vid.Slot() <= 1 {
+			//	//	TrackedVertices.TrackPointerNotGCed(vid, vid.IDShortString(), 5*time.Second)
+			//	//}
+			//}
 			return true
 		}
 		return false

core/vertex/past_cone.go
@@ -12,7 +12,6 @@ import (
 	"github.com/lunfardo314/proxima/util"
 	"github.com/lunfardo314/proxima/util/lines"
 	"github.com/lunfardo314/proxima/util/set"
-	"github.com/lunfardo314/proxima/util/trackgc"
 	"golang.org/x/exp/maps"
 )
 
@@ -61,20 +60,20 @@ func _nextPastConeBaseCounter() (ret int) {
 	return
 }
 
-var (
-	trackedPastConeBases = trackgc.New[PastConeBase](
-		func(p *PastConeBase) string {
-			return fmt.Sprintf("%T: %p #%d", p, p, p.num)
-		}, func(p *PastConeBase) string {
-			return fmt.Sprintf("%T: %p #%d\n%s", p, p, p.num, p.Lines("    ").String())
-		})
-	trackedPastCones = trackgc.New[PastCone](
-		func(p *PastCone) string {
-			return fmt.Sprintf("%T: %p #%d", p, p, p.num)
-		}, func(p *PastCone) string {
-			return fmt.Sprintf("%T: %p #%d, oldest ref: %s", p, p, p.num, p.OldestReference().String())
-		})
-)
+//var (
+//	trackedPastConeBases = trackgc.New[PastConeBase](
+//		func(p *PastConeBase) string {
+//			return fmt.Sprintf("%T: %p #%d", p, p, p.num)
+//		}, func(p *PastConeBase) string {
+//			return fmt.Sprintf("%T: %p #%d\n%s", p, p, p.num, p.Lines("    ").String())
+//		})
+//	trackedPastCones = trackgc.New[PastCone](
+//		func(p *PastCone) string {
+//			return fmt.Sprintf("%T: %p #%d", p, p, p.num)
+//		}, func(p *PastCone) string {
+//			return fmt.Sprintf("%T: %p #%d, oldest ref: %s", p, p, p.num, p.OldestReference().String())
+//		})
+//)
 
 const (
 	FlagPastConeVertexKnown             = FlagsPastCone(0b00000001) // each vertex of consideration has this flag on

core/work_process/tippool/tippool.go
@@ -90,13 +90,13 @@ func (t *SequencerTips) consume(inp Input) {
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
 
-	{ // debug
-		for _, lm := range t.latestMilestones {
-			if lm.Slot() <= 1 {
-				t.Log().Infof(">>>>>>>>>>> in tippool %s", lm.WrappedTx.IDShortString())
-			}
-		}
-	}
+	//{ // debug
+	//	for _, lm := range t.latestMilestones {
+	//		if lm.Slot() <= 1 {
+	//			t.Log().Infof(">>>>>>>>>>> in tippool %s", lm.WrappedTx.IDShortString())
+	//		}
+	//	}
+	//}
 
 	t.updateLatestSequencerData(inp.WrappedTx, *seqID)
 

sequencer/own_milestones.go
@@ -121,7 +121,7 @@ func (seq *Sequencer) purgeOwnMilestones(ttl time.Duration) (int, int) {
 			delete(seq.ownMilestones, vid)
 			vid.UnReference()
 			count++
-			seq.Log().Infof("--------- deleted own milestone %s, outputs from past cone %d ------------", vid.IDShortString(), len(withTime.consumed))
+			//seq.Log().Infof("--------- deleted own milestone %s, outputs from past cone %d ------------", vid.IDShortString(), len(withTime.consumed))
 		}
 	}
 

sequencer/sequencer.go
@@ -21,7 +21,6 @@ import (
 	"github.com/lunfardo314/proxima/util"
 	"github.com/lunfardo314/proxima/util/checkpoints"
 	"github.com/lunfardo314/proxima/util/set"
-	"github.com/lunfardo314/proxima/util/trackgc"
 	"go.uber.org/zap"
 )
 
@@ -92,9 +91,9 @@ type (
 
 const TraceTag = "sequencer"
 
-var TrackGCSequencers = trackgc.New[Sequencer](func(p *Sequencer) string {
-	return p.SequencerName()
-})
+//var TrackGCSequencers = trackgc.New[Sequencer](func(p *Sequencer) string {
+//	return p.SequencerName()
+//})
 
 func New(env Environment, seqID ledger.ChainID, controllerKey ed25519.PrivateKey, opts ...ConfigOption) (*Sequencer, error) {
 	cfg := configOptions(opts...)

commit 856927cc775e46884abd07186e7275ad71a44abe

memdag: branches must not retain their past cone (fixes unbounded leak)
A branch is a committed ledger state, served via the state reader, so its
WrappedTx must hold no past-cone dependencies. But branch wrap-up did
ConvertToDetached() (nils pastCone) then immediately
SetTxStatusGood(PastConeBase.CloneImmutable(), cov), re-attaching a clone
whose vertices map holds STRONG refs to every vertex in the past cone
(regression from bf4477f5, Mar 2025; the branch path stored nil before).

This pinned the whole past cone in memory and, because branch vertices are
never trimmed from a past cone (CheckAndClean phase 2) and MergePastCone
copies a predecessor's whole vertex set forward, every milestone accumulated
the entire branch chain back to the oldest in-memory branch, propagated
forever via CloneImmutable. Observed on testnet: proxima_past_cone_size grew
to ~= memDAG size, both climbing unbounded since restart, GC oldestSlot frozen
at the restart slot with deleted~=0 (vertices detached but pinned as strong
map keys). The earlier 4bfd7041 consumed-drop addressed a different forward-ref
pin and did not help.

Fix: add WrappedTx.SetTxStatusGoodBranch(coverage) — marks the branch Good and
stores only the coverage (still read for LRB selection / tippool / sequencer),
leaving pastCone == nil. Branch wrap-up uses it instead of SetTxStatusGood with
a clone. Baseline/ancestor branches met in a successor's walk now resolve via
the state reader (BranchKnowsTransaction), not by merging a stored past cone.
Tighten the convertToDetached DetachedVertex assertion to pastCone == nil and
refresh the now-stale comments.

go test ./core/... green; tests/ branch-heavy suite 5/5 green (clean develop
flakes ~40% on a pre-existing wrap-up detach/reattach race, unaffected here).

core/attacher/attacher_milestone.go
@@ -173,11 +173,14 @@ func (a *milestoneAttacher) run() error {
 
 	a.pastCone.SetFlagsUp(a.vid, vertex.FlagPastConeVertexDefined)
 	if a.vid.IsBranchTransaction() {
-		// branch transaction vertex is immediately detached. Thus branch transaction does not reference the past cone
+		// branch transaction vertex is immediately detached and does NOT retain the past cone:
+		// a branch is a committed state, served via the state reader. Storing only the coverage
+		// (not a CloneImmutable of the past cone) avoids pinning the past cone in memory and
+		// prevents it from being merged into successors' past cones (the memDAG leak).
 		a.Tracef(vertex.TraceTagPastConeDiag, "DETACH (branch wrapup): vid=%s pastConeSize=%d",
 			a.vid.IDShortString, a.pastCone.PastConeBase.Len())
 		a.vid.ConvertToDetached()
-		a.vid.SetTxStatusGood(a.pastCone.PastConeBase.CloneImmutable(), a.FinalLedgerCoverage(a.vid.Timestamp()))
+		a.vid.SetTxStatusGoodBranch(a.FinalLedgerCoverage(a.vid.Timestamp()))
 		a.EvidenceBranchMutations(a.finals.MutationStats.NumCreated + a.finals.MutationStats.NumDeleted)
 		// branch wrap-up freed a lot of state — nudge the async GC worker. Non-blocking:
 		// the worker decides whether to actually runtime.GC() based on heap threshold + rate limit.

core/vertex/vid.go
@@ -102,9 +102,9 @@ func (vid *WrappedTx) ReattachVertexNoLock(tx *transaction.Transaction) {
 
 // ConvertToDetached detaches past cone and leaves only a collection of produced outputs
 // Detaches input dependencies and converts to the DetachedVertex
-// Note, however, that for branches, WrappedTx with DetachedVertex can later contain reference to the pastCone structure.
 // Upon repeated calls to ConvertToDetached we set pastCone to nil. If not this, DAG remains always connected and
-// old vertices are not garbage collected -> memory leak!
+// old vertices are not garbage collected -> memory leak! Branches never retain a past cone (see
+// SetTxStatusGoodBranch), so a detached vertex always has pastCone == nil.
 func (vid *WrappedTx) ConvertToDetached() {
 	vid.convertToDetached(false)
 }
@@ -133,8 +133,7 @@ func (vid *WrappedTx) convertToDetached(force bool) {
 			detached = true
 		},
 		DetachedVertex: func(v *DetachedVertex) {
-			util.Assertf(vid.pastCone == nil || vid.IsBranchTransaction(), "vid.pastCone == nil ||vid.IsBranchTransaction()")
-			vid.pastCone = nil // Important: if not this, memdag leaks memory. Later: why exactly?
+			util.Assertf(vid.pastCone == nil, "vid.pastCone == nil")
 			detached = true
 		},
 		VirtualTx: func(v *VirtualTransaction) {
@@ -228,6 +227,24 @@ func (vid *WrappedTx) SetTxStatusGoodNoLock(pastCone *PastConeBase, coverage uin
 		// leaving nil, which would be indistinguishable from the reattach race.
 		vid.coverage.Store(util.Ref(coverage))
 	}
+}
+
+// SetTxStatusGoodBranch marks a branch vertex Good, storing only its ledger coverage and
+// NOT retaining a past cone. A branch is a committed ledger state, served via the state
+// reader, so it must not hold past-cone dependencies: retaining them pins the whole past
+// cone in memory (strong refs in PastConeBase.vertices) and lets it be merged into
+// successors' past cones, which together accumulate the entire branch chain and leak the
+// memDAG. The coverage is still stored because it is read for LRB selection / sequencer
+// logic (cmp.go, tippool, sequencer/task).
+func (vid *WrappedTx) SetTxStatusGoodBranch(coverage uint64) {
+	vid.mutex.Lock()
+	defer vid.mutex.Unlock()
+
+	util.Assertf(vid.IsBranchTransaction(), "SetTxStatusGoodBranch: not a branch %s", vid.IDShortString)
+	util.Assertf(vid.GetTxStatusNoLock() != Bad, "vid.GetTxStatusNoLock() != Bad (%s)", vid.StringNoLock)
+
+	vid.flags.SetFlagsUp(FlagVertexDefined | FlagVertexIgnoreAbsenceOfPastCone)
+	vid.coverage.Store(util.Ref(coverage))
 }
 
 func (vid *WrappedTx) SetSequencerAttachmentFinished() {