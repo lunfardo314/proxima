4216a686a7de9f2436a7625fddd01d1410cb1ba0
HM4704 <hermann.moosburger@gmail.com>
2026-08-23 19:23:21 +0200

    always build before run

--- tests/node-docker-setup/run.sh
+++ tests/node-docker-setup/run.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-if [ ! -f "proxi" ]; then
-    # we need local proxi for update-snapshot.sh, build the image
-    ./build.sh
-fi 
+./build.sh
 
 if [ ! -d "./data/config" ]; then
     mkdir -p ./data/config
     chmod -R 777 ./data
 fi
-
 
 docker compose up
